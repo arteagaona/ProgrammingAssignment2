1bc7bb65d2a2d9b025eddc3ab09c674018e99274
arteagaona <arteaga@ciencias.unam.mx>
2017-08-11 23:18:42 -0500

    Implementation for makeCacheMatrix function

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,21 @@
 ## Implementation of two functions that are used to create a special
 ## object that stores a matrix -assumed invertible- and cache its inverse
 
-## Write a short comment describing this function
+## Creates a list containing a function to set and get the value of the
+## matrix and its inverse -here we assume the matrix is invertible-
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    inverse <- NULL
+    set <- function(y) {
+        x <<- y
+        inverse <<- NULL
+    }
+    get <- function() x
+    setInverse <- function(solve) inverse <<- solve
+    getInverse <- function() inverse
+    list(set = set, get = get,
+         setInverse = setInverse,
+         getInverse = getInverse)
 }
 
 
